fbbfd78ab18578d5d14df56eaf17c2b07affe60e
wallace <wallace_wolv@hotmail.com>
2023-09-11 21:47:44 -0300

    feat: Adicionando botão e layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { Button } from "./components/ui/button";
 
 export function App() {
   return (
-    <h1>Hello World</h1>
-  )
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 no NLW da Rocketseat
+          </span>
+          <Button variant="outline">Github</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
