c9751a3bd95b59e8cb3159efaf99332ef572c90f
wallace <wallace_wolv@hotmail.com>
2023-09-13 19:50:49 -0300

    feat: Criando funcionalidade para setar o estado do input quando selecionar um arquivo

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -4,18 +4,46 @@ import { Button } from "./ui/button";
 import { Label } from "./ui/label";
 import { Separator } from "./ui/separator";
 import { Textarea } from "./ui/textarea";
+import { ChangeEvent, useState } from "react";
 
 export function VideoInputForm() {
+  const [videoFile, setVideoFile] = useState<File | null>(null);
+
+  function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget;
+
+    if (!files) {
+      return;
+    }
+
+    const selectedFile = files[0];
+
+    setVideoFile(selectedFile);
+  }
+
   return (
     <form className="space-y-6">
       <label
         htmlFor="video"
         className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
       >
-        <FileVideo className="w-4 h-4" />
-        Selecione um vídeo
+        {videoFile ? (
+          "oi"
+        ) : (
+          <>
+            <FileVideo className="w-4 h-4" />
+            Selecione um vídeo
+          </>
+        )}
       </label>
-      <input type="file" id="video" accept="video/mp4" className="sr-only" />
+
+      <input
+        type="file"
+        id="video"
+        accept="video/mp4"
+        className="sr-only"
+        onChange={handleFileSelected}
+      />
 
       <Separator />
 
